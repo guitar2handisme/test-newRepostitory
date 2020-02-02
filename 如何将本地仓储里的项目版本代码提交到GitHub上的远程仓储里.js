4568ba9d76ab8fda445d002f24c89a0072f80544
wangpeng <18594227104@163.com>
2020-02-03 01:04:36 +0800

    master-补充笔记

--- 如何将本地仓储里的项目版本代码提交到GitHub上的远程仓储里.js
+++ 如何将本地仓储里的项目版本代码提交到GitHub上的远程仓储里.js
@@ -1,5 +1,6 @@
 // 问: 如何将本地仓储里的项目版本代码提交到GitHub上的远程仓储里?
-// 答: 输入[git push GitHub上远程仓储的https的url 所有存放的远程仓储的分支名]
+// 答: 先要提交到[git本地仓储]然后在去[GitHub]官网新建一个用于存储该项目代码的[远程仓储]
+//     最后在输入[git push GitHub上远程仓储的https的url 所要存放的远程仓储的分支名]
 
 // master-1
 // master-2
